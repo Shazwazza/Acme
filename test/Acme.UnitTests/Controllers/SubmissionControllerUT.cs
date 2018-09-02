661d6afb8de80baf221f921f73c19c7fc56e939b
Bjarke Berg <mail@bergmania.dk>
2018-09-02 21:09:49 +0200

    reference change

--- test/Acme.UnitTests/Controllers/SubmissionControllerUT.cs
+++ test/Acme.UnitTests/Controllers/SubmissionControllerUT.cs
@@ -1,28 +1,22 @@
-using System.Collections.Generic;
 using System.Threading;
-using System.Threading.Tasks;
-using Acme.Application.Interfaces;
 using Acme.Domain.Models;
-using Acme.Presentation.Web.Controllers;
+using Acme.Presentation.Website.Controllers;
 using Acme.UnitTests.Helpers;
-using AutoFixture.NUnit3;
 using Microsoft.AspNetCore.Mvc;
-using Moq;
 using NUnit.Framework;
 
 namespace Acme.UnitTests.Controllers
 {
     public class SubmissionControllerUT
     {
-
         [Test]
         [AutoMoqData]
         public void List_Returns_paging_result(
-            SubmissionController sut, 
+            SubmissionController sut,
             PagingInformation pagingInformation)
         {
             var response = sut.List(pagingInformation, CancellationToken.None);
-     
+
             Assert.IsInstanceOf<ActionResult<PagingResult<Submission>>>(response.Result);
         }
     }
